90e82283ace289d1b94fe2c9dddb89cc9e583f03
proxy0001 <g.proxy0001@gmail.com>
2016-08-14 12:48:44 +0800

    add: single page for post,#2

--- views/post/index.js
+++ views/post/index.js
@@ -6,13 +6,23 @@
  * /post/:id
  */
 exports.readOneById = function (req, res) {
+  console.log(req.params.id);
   req.app.db.models.Post.findOne({
     _id: req.params.id
   }, function (err, post) {
-    console.log(post.html);
+    console.log(post);
     res.render('post/single', {
       subject: post.subject,
       html: post.html
-    })
+    });
   });
-}
+  // req.app.db.models.Post.findOne({
+  //   _id: req.params.id
+  // }, function (err, post) {
+  //   console.log(post.html);
+  //   res.render('post/single', {
+  //     subject: post.subject,
+  //     html: post.html
+  //   });
+  // });
+};
